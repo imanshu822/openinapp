7bb15cc15b7d56ad538b770c4ce355801c2c644f
Anshu <imanshu822@gmail.com>
2024-01-30 23:41:07 +0530

    firebase credintial chnged

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,27 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyAM_xvaGvoR8qEmVfMLtvMTuGC6BUo7uQ0",
   apiKey: "AIzaSyAD1gZ7I4hGSzFOWnsYL8KWvT4mr8lRt3I",
-
-  // authDomain: "openinapp-a91e1.firebaseapp.com",
   authDomain: "openinapp-86828.firebaseapp.com",
-
-  // projectId: "openinapp-a91e1",
   projectId: "openinapp-86828",
-
-  // storageBucket: "openinapp-a91e1.appspot.com",
   storageBucket: "openinapp-86828.appspot.com",
-
-  messagingSenderId: "907135680595",
-
-  appId: "1:907135680595:web:ed20336fbe90923ead86a6",
-
-  measurementId: "G-E1BH51FRPD",
+  messagingSenderId: "689663004625",
+  appId: "1:689663004625:web:c8a3e72c715ded61031af0",
+  measurementId: "G-E0DC396NWL",
 };
 
-// Initialize Firebase
+// // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
